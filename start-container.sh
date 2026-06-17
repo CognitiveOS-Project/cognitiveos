a89ddcfd73a12908906c27197130f11a932a841a
Jean Machuca <correojean@gmail.com>
2026-06-16 22:24:58 -0400

    add a reset script and fix project path in start-container

--- start-container.sh
+++ start-container.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+PROJECT_PATH="$(cd "$(dirname "$0")" && pwd)"
+
 echo "🚀 Launching the container..."
+
+mkdir -p "$PROJECT_PATH/models"
 
 docker build -t aiworkspace_workspace . && docker run -it \
   --name aiworkspace_container \
-   --privileged \
-  -v $(pwd):/workspace \
-  -v $(pwd)/models:/workspace/models \
+  --privileged \
+  -v "$PROJECT_PATH:/workspace" \
   -v /var/run/docker.sock:/var/run/docker.sock \
   aiworkspace_workspace
